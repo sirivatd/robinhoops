b56dcdeb278ba87c5c15e7a046fcb573674b58a6
sirivat <dsirivat17@gmail.com>
2018-10-12 20:53:30 -0700

    Implement account drop down menu

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -6,12 +6,43 @@ import UserStocksIndex from "./user_stocks/user_stocks_index";
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.showMenu = this.showMenu.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchAllOrders(this.props.currentUser.id);
+    document.addEventListener("mousedown", this.handleClick, false);
   }
 
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClick, false);
+  }
+
+  showMenu() {
+    const menu = document.getElementsByClassName("account-settings-menu")[0];
+    menu.classList.toggle("hidden-menu");
+  }
+
+  handleClick(e) {
+    console.log(document.getElementsByClassName("account-settings-menu")[0]);
+    if (
+      document
+        .getElementsByClassName("account-settings-menu")[0]
+        .contains(e.target)
+    ) {
+      return;
+    }
+    this.handleClickOutside();
+  }
+
+  handleClickOutside() {
+    const menu = document.getElementsByClassName("account-settings-menu")[0];
+    if (menu.classList.contains("hidden-menu")) {
+    } else {
+      menu.classList.toggle("hidden-menu");
+    }
+  }
   render() {
     let currentUser = this.props.currentUser;
     const loader = () => (
@@ -36,19 +67,43 @@ class Home extends React.Component {
       />
     );
 
+    const accountSettings = () => (
+      <div className="account-settings-menu hidden-menu">
+        <div className="account-settings-stats">
+          <h3 className="account-settings-name">Don Sirivat</h3>
+          <div className="account-setting-port">
+            <h4 className="account-setting-port-value">$12,034.12</h4>
+
+            <h4 className="account-setting-label">Portfolio Value</h4>
+          </div>
+
+          <div className="account-setting-power">
+            <h4 className="account-setting-power-value">$1524.23</h4>
+            <h4 className="account-setting-label">Buying Power</h4>
+          </div>
+        </div>
+        <hr className="account-setting-divider" />
+        <ul
+          className="account-settings-list"
+          onClick={() => this.props.logout(currentUser)}
+        >
+          <li className="account-settings-item">Logout</li>
+        </ul>
+      </div>
+    );
+
     return (
       <div className="home-section">
+        {accountSettings()}
         <div className="fixed-nav-bar">
           <img
             className="logo-img"
             src="https://media.glassdoor.com/sqll/1167765/robinhood-squarelogo-1530549970728.png"
           />
           <nav className="login-signup">
-            <button
-              className="login-logout-button"
-              onClick={() => this.props.logout(currentUser)}
-            >
-              Logout
+            <button className="login-logout-button">Leaderboard</button>
+            <button className="login-logout-button" onClick={this.showMenu}>
+              Account
             </button>
           </nav>
         </div>
