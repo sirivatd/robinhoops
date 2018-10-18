126660ac8aa7e0b1014f3f01d57ddcf6fe382e09
sirivat <dsirivat17@gmail.com>
2018-10-17 20:59:57 -0700

    Add new routes for graph data

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
       resources :orders, only: [:create, :index, :show, :update]
       resources :athletes, only: [:show]
       resources :users_port_snapshots, only: [:index, :create]
+      resources :watchlist_items, only: [:index, :create, :destroy]
     end
     resource :session, only: [:create, :destroy, :show]
     resources :stocks, only: [:index, :show]
     resources :athletes, only: [:show, :index]  do
       resources :tweets, only: [:index]
+      resources :tweet_score_snapshots, only: [:index]
+      resources :athlete_price_snapshots, only: [:index]
 
     end
   end
